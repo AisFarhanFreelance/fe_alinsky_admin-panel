184be2f4ad77f698e676c54a0b876c652a7951c5
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-07-23 23:27:19 +0700

    fix: utils change name to more descriptive

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utilities";
 import * as LabelPrimitive from "@radix-ui/react-label";
 import * as React from "react";
 
