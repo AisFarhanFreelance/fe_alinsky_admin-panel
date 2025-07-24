e95303c32fa2a41560d45784b1e7800f7d4090e8
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2025-07-24 22:59:15 +0700

    feat: add sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import { LoginForm } from "./components/login-form";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <LoginForm />
-    </div>
-  );
+  redirect("/dashboard");
 }
