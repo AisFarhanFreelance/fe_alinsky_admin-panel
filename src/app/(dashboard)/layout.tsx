9edc504b86e9f593051483ed9ef588f19f8cc5be
Aldi Himawan <62284108+Aldi-H@users.noreply.github.com>
2025-07-25 11:09:12 +0700

    Feat/sidebar navigation (#2)
    * Add CODEOWNERS file to define repository ownership (#1)
    
    * Add new font files for Helvetica and Satoshi families
    
    * Add new SVG logos for Alinsky project
    
    - Created alinsky-icon.svg with a design featuring a blue background and white elements.
    - Added alinsky-logo.svg with a pattern fill and embedded base64 image for branding purposes.
    
    * Remove duplicate import of eslintPluginUnicorn in eslint.config.mjs
    
    * Refactor import statement for global styles in layout component
    
    * feat: login form ui v0.dev
    
    * fix: utils change name to more descriptive
    
    * feat: add sidebar
    
    ---------
    
    Co-authored-by: Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,23 @@
+import type React from "react";
+
+import { Header } from "@/components/sidebar/header";
+import { Sidebar } from "@/components/sidebar/sidebar";
+import { SidebarProvider } from "@/components/sidebar/sidebar-provider";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <div className="min-h-screen bg-background">
+        <Sidebar />
+        <div className="lg:pl-72">
+          <Header />
+          <main className="p-4 md:p-6 lg:p-8">{children}</main>
+        </div>
+      </div>
+    </SidebarProvider>
+  );
+}
