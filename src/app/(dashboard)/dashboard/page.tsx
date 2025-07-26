fe1d81b29eb52084c1d64e0093987c63e393d8a7
Aldi-H <aldihimawan88@gmail.com>
2025-07-26 12:31:21 +0700

    feat: restructure dashboard layout and integrate DashboardTabs component

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,21 @@
+import DashboardTabs from "@/components/tabs/dashboard-tabs";
+
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      Dashboard
+    <div className="min-h-screen flex flex-col gap-4">
+      <div>
+        <h1 className="text-2xl md:text-3xl font-bold tracking-tight">
+          Dashboard
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam itaque
+          corporis atque!
+        </p>
+      </div>
+
+      <div>
+        <DashboardTabs />
+      </div>
     </div>
   );
 }
